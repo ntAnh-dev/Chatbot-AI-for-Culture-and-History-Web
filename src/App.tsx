622574befca44d839ed3ee209ff019d0030eac14
ntAnh-dev <anhnt2112.dev@gmail.com>
2025-04-10 09:56:24 +0700

    Add topbar and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import ChatWindow from './components/Chat'
+import ChatWindow from "./components/Chat"
+import Topbar from "./components/Topbar"
 
 function App() {
 
   return (
-    <ChatWindow />
+    <div className='w-screen h-dvh flex relative bg-[#212121]'>
+      <div className="flex-grow h-full flex flex-col">
+        <Topbar />
+        <ChatWindow />
+      </div>
+    </div>
   )
 }
 
 export default App
+
+
+//         | Topbar
+// Sidebar |_________
+//         | Main
